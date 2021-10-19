f6039d999fb209874cf0f2b7e63a45553afba0b9
R3omar <53862608+R3omar@users.noreply.github.com>
2021-10-19 19:13:45 +0200

    Removed free choice

--- src/main/java/com/re0mar/ooad/KennisToets.java
+++ src/main/java/com/re0mar/ooad/KennisToets.java
@@ -1,12 +1,9 @@
 package com.re0mar.ooad;
 
 import com.re0mar.ooad.Punten.BasicPuntenSysteem;
-import com.re0mar.ooad.antwoorden.kortAntwoord;
-import com.re0mar.ooad.antwoorden.meerkeuzeAntwoord;
+import com.re0mar.ooad.antwoorden.*;
 import com.re0mar.ooad.taal.ITaal;
-import com.re0mar.ooad.vragen.Vraag;
-import com.re0mar.ooad.vragen.kortVraag;
-import com.re0mar.ooad.vragen.meerkeuzeVraag;
+import com.re0mar.ooad.vragen.*;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -14,19 +11,17 @@ import java.util.Scanner;
 
 public class KennisToets {
 
-    private final ArrayList<Vraag> vragenSet = new ArrayList<>();
-    private ITaal taal;
-    private String code;
-    private String naam;
+    private final ArrayList<Vraag> vragenSet;
+    private final ITaal taal;
+    private final String code;
+    private final String naam;
 
-    public KennisToets(ITaal taal, String code, String naam) {
+    public KennisToets(ITaal taal, String code, String naam, ArrayList<Vraag> vragen) {
         this.taal = taal;
         this.code = code;
         this.naam = naam;
-        vragenSet.add(new meerkeuzeVraag(taal, new meerkeuzeAntwoord(1), "Wat is A", new BasicPuntenSysteem(6), new String[]{"b", "c", "0", "A", "c", "0", "A", "c", "0", "A", "c", "0", "A", "c", "0", "A", "c", "0", "A", "c", "0", "A"}));
-        vragenSet.add(new kortVraag(taal, new kortAntwoord(new String[]{"antwoord", "answer"}), "Type answer", new BasicPuntenSysteem(9)));
-        vragenSet.add(new kortVraag(taal, new kortAntwoord(new String[]{"antwoord", "answer"}), "Type answer", new BasicPuntenSysteem(9)));
-    }
+        this.vragenSet = vragen;
+        }
 
     public void doeKennisToets() {
         int punten = 0;
--- src/main/java/com/re0mar/ooad/provoApplication.java
+++ src/main/java/com/re0mar/ooad/provoApplication.java
@@ -1,6 +1,12 @@
 package com.re0mar.ooad;
 
+import com.re0mar.ooad.Punten.BasicPuntenSysteem;
+import com.re0mar.ooad.antwoorden.kortAntwoord;
+import com.re0mar.ooad.antwoorden.meerkeuzeAntwoord;
 import com.re0mar.ooad.taal.Nederlands;
+import com.re0mar.ooad.vragen.Vraag;
+import com.re0mar.ooad.vragen.kortVraag;
+import com.re0mar.ooad.vragen.meerkeuzeVraag;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -13,9 +19,15 @@ public class provoApplication {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        toetsen.add(new KennisToets(new Nederlands(), "STE23", "ToetsA"));
-        toetsen.add(new KennisToets(new Nederlands(), "TER45", "ToetsB"));
-        toetsen.add(new KennisToets(new Nederlands(), "UIS12", "ToetsC"));
+        ArrayList<Vraag> vragenSet = new ArrayList<>();
+        vragenSet.add(new meerkeuzeVraag(new Nederlands(), new meerkeuzeAntwoord(1), "Wat is A", new BasicPuntenSysteem(6), new String[]{"b", "c", "0", "A", "c"}));
+        vragenSet.add(new kortVraag(new Nederlands(), new kortAntwoord(new String[]{"antwoord", "answer"}), "Type answer", new BasicPuntenSysteem(9)));
+        vragenSet.add(new kortVraag(new Nederlands(), new kortAntwoord(new String[]{"antwoord", "answer"}), "Type answer", new BasicPuntenSysteem(9)));
+
+
+        toetsen.add(new KennisToets(new Nederlands(), "STE23", "ToetsA", vragenSet));
+        toetsen.add(new KennisToets(new Nederlands(), "TER45", "ToetsB", vragenSet));
+        toetsen.add(new KennisToets(new Nederlands(), "UIS12", "ToetsC", vragenSet));
 
         System.out.println("Welkom bij Provo, om een toets te maken voer een toetscode in");
         System.out.println("Om alle aanwezige toetsen te zien voer ? in");
